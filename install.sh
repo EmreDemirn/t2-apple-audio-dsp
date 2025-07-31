bcf1fdf905b16ae4f3e2293c98566a08d16a410c
lemmyg <lemmygl@protonmail.com>
2025-07-31 22:16:47 +0100

    new mic with triforce plug, adjustment for speakers

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ echo "Install Macbook Pro 16.1 speakers DSP config as root"
 echo "Copying 10-t2_161_speakers.conf to /etc/pipewire/pipewire.conf.d"
 sudo mkdir -p /etc/pipewire/pipewire.conf.d
 sudo cp  config/10-t2_161_speakers.conf /etc/pipewire/pipewire.conf.d
+echo "Copying 10-t2_161_mic.conf to /etc/pipewire/pipewire.conf.d"
+sudo cp  config/10-t2_161_mic.conf /etc/pipewire/pipewire.conf.d
 echo "Copying firs/*.wav to /usr/share/pipewire/devices/apple"
 sudo mkdir -p /usr/share/pipewire/devices/apple
 sudo cp firs/*.wav /usr/share/pipewire/devices/apple
