d616940f2f50a92cfe7809b819a37df88608aa60
EmreDemirn <112325107+EmreDemirn@users.noreply.github.com>
2025-12-10 19:04:40 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # SPDX-License-Identifier: MIT
 # (C) 2023 Linux T2 Kernel Team
-echo "Install Macbook Pro 16.1 speakers DSP config as root"
-echo "Copying 10-t2_161_speakers.conf to /etc/pipewire/pipewire.conf.d"
+# EmreDemirn
+echo "Install Macbook Pro 16,2 speakers DSP config as root"
+echo "Copying 40-apple-dsp-speakers.conf to /etc/pipewire/pipewire.conf.d"
 sudo mkdir -p /etc/pipewire/pipewire.conf.d
-sudo cp  config/10-t2_161_speakers.conf /etc/pipewire/pipewire.conf.d
+sudo cp  config/40-apple-dsp-speakers.conf /etc/pipewire/pipewire.conf.d
 echo "Copying firs/*.wav to /usr/share/pipewire/devices/apple"
 sudo mkdir -p /usr/share/pipewire/devices/apple
 sudo cp firs/*.wav /usr/share/pipewire/devices/apple
